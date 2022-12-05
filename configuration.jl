33f02f1dd6dbb8436d3741bf82e307fd1730c5fd
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2022-12-05 12:43:21 -0500

    More extensive configuration

--- configuration.jl
+++ configuration.jl
@@ -1,8 +1,11 @@
+using Oceananigans
+
 config = Dict(
 
     :directories => Dict(
         :ecco_data_dir => "/storage6/alir/ecco4v4",
-        :figures_dir => "/storage6/alir/RealityInspiredOceanTurbulary"
+        :figures_dir => "/storage6/alir/RealityInspiredOceanTurbulary/site_South_of_Durban/figures",
+        :simulation_output_dir => "/storage6/alir/RealityInspiredOceanTurbulary/site_South_of_Durban/data",
     ),
 
     :constants => Dict(
@@ -12,11 +15,25 @@ config = Dict(
     ),
 
     :site => Dict(
-        :name => "South of Durban",
-        :latitude => -50,
-        :longitude => 30,
+        :name => "South_of_Durban",
+        :latitude => -35,
+        :longitude => 32,
         :start_date => Date(2014, 01, 01),
-        :end_date => Date(2014, 01, 31)
-    )
+        :end_date => Date(2014, 12, 31)
+    ),
+
+    :architecture => CPU(),
+
+    :grid => Dict(
+        :Nx => 128,
+        :Ny => 128,
+        :Nz => 128,
+        :Lx => 500,
+        :Ly => 500,
+        :Lz => 500
+    ),
+
+    :plot_site_forcing => false,
+    :animate_site_profiles => false
 
 )
